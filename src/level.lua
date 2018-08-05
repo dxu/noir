95d0c23790be6249d66832f86a54ab01b057d6fe
David <xudavidb@gmail.com>
2018-08-04 21:22:01 -0400

    add level updates

--- src/level.lua
+++ src/level.lua
@@ -7,11 +7,27 @@ level = scene:new()
 function level:initialize()
 -- create the player and the platforms
 	local player = player:new()
+  self.gravity = 2
 	self:add(player)
 end
 
 function level:update()
--- update logic for a level on each tick
+  -- update gravity
+	for k,ent in pairs(self.entities) do
+    if not ent.grounded then
+      local new_position = ent.y + self.gravity
+      -- if new_position and the ent's height go below ground,
+      -- move them back up and don't modify
+      if ent.y + ent.height >= 128 then
+        ent.y = 128 - ent.height
+        ent.vel_y = 0
+        ent.acc_y = 0
+        ent.grounded = true
+      else
+        ent.y = new_position
+      end
+    end
+  end
 end
 
 function level:draw()
